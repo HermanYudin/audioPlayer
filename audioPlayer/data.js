d00fc41cda6a06bb6bcf0b7578ad0c93356e89f2
HermanYudin <hermanhockey1994@yandex.ru>
2023-09-20 00:54:20 +0300

    feat:work is done (Wed, 20 Sep, 2023, 0:47 AM)

---
+++ audioPlayer/data.js
@@ -0,0 +1,35 @@
+let songs = [
+    {
+        name: 'Big Guns',
+        path: 'music/Skid Row - Big Guns.mp3',
+        artist: 'Skid Row',
+        cover: 'covers/skid.jpg'
+    },
+    {
+        name: 'The RocknRoll Qeen',
+        path: 'music/The Subways - The RocknRoll Qeen.mp3',
+        artist: 'The Subways',
+        cover: 'covers/sub.jpg'
+    },
+    {
+        name: 'I Got Mine',
+        path: 'music/The Black Keys - I Got Mine.mp3',
+        artist: 'The Black Keys',
+        cover: 'covers/black.jpg'
+    },
+    {
+        name: 'Time of Dying',
+        path: 'music/Three Days Grace - Time of Dying.mp3',
+        artist: 'Three Days Grace',
+        cover: 'covers/time.jpg'
+    },
+    {
+        name: 'Сентябрь Горит',
+        path: 'music/Stigmata - Сентябрь Горит.mp3',
+        artist: 'Stigmata',
+        cover: 'covers/sept.jpg'
+    }
+];
+
+
+
--- audioPlayer/index.js
+++ audioPlayer/index.js
@@ -1,32 +1,105 @@
-let songs = [
-    {
-        name: 'Big Guns',
-        path: 'music/Skid Row - Big Guns.mp3',
-        artist: 'Skid Row',
-        cover: 'covers/skid.jpg'
-    },
-    {
-        name: 'The RocknRoll Qeen',
-        path: 'music/The Subways - The RocknRoll Qeen.mp3',
-        artist: 'The Subways',
-        cover: 'covers/sub.jpg'
-    },
-    {
-        name: 'I Got Mine',
-        path: 'music/The Black Keys - I Got Mine.mp3',
-        artist: 'The Black Keys',
-        cover: 'covers/black.jpg'
-    },
-    {
-        name: 'Time of Dying',
-        path: 'music/Three Days Grace - Time of Dying.mp3',
-        artist: 'Three Days Grace',
-        cover: 'covers/time.jpg'
-    },
-    {
-        name: 'Сентябрь Горит',
-        path: 'music/Stigmata - Сентябрь Горит.mp3',
-        artist: 'Stigmata',
-        cover: 'covers/sept.jpg'
+let currentMusic = 0;
+const music = document.querySelector('#audio');
+const seekBar = document.querySelector('.seek-bar');
+const songName = document.querySelector('.music-name');
+const artistName = document.querySelector('.artist-name');
+const disk = document.querySelector('.disk');
+const currentTime = document.querySelector('.current-time');
+const musicDuration = document.querySelector('.song-duration');
+const playBtn = document.querySelector('.play-btn');
+const forwardBtn = document.querySelector('.forward-btn');
+const backwardBtn = document.querySelector('.backward-btn');
+
+
+// --------------Кликеры---------------
+
+playBtn.addEventListener('click', function(){
+    if(playBtn.classList.contains('pause')){
+        music.play();
     }
-]
+    else{
+        music.pause(); 
+    }
+    playBtn.classList.toggle('pause');
+    disk.classList.toggle('play');
+});
+
+forwardBtn.addEventListener('click', function(){
+    if(currentMusic >= songs.length - 1){
+        currentMusic = 0;
+    }
+    else{
+        currentMusic ++;
+    }
+    setMusic(currentMusic);
+    playMusic();
+});
+
+backwardBtn.addEventListener('click', function(){
+    if(currentMusic <= 0){
+        currentMusic = songs.length - 1;
+    }
+    else{
+        currentMusic --;
+    }
+    setMusic(currentMusic);
+    playMusic();
+});
+
+// ------------Ставим по i музыку, обложку и название-----
+
+const setMusic = function(i) {
+    seekBar.value = 0;
+    let song = songs[i];
+    currentMusic = i;
+    music.src = song.path;
+
+    songName.innerHTML = song.name;
+    artistName.innerHTML = song.artist;
+    disk.style.backgroundImage = `url('${song.cover}')`;
+
+    currentTime.innerHTML = '00:00';
+
+    setTimeout(function(){//без этого считать продолжительность песни не будет
+    seekBar.max = music.duration;
+    musicDuration.innerHTML = formatTime(music.duration);
+    },300);
+}
+
+setMusic(0);
+
+// -------------Форматируем время песни в минуты и секунды---
+
+const formatTime = (time) => {
+    let min = Math.floor(time / 60);
+    if(min < 10){
+        min = `0${min}`;
+    }
+    let sec = Math.floor(time % 60);
+    if(sec < 10){
+        sec = `0${sec}`;
+    }
+    return `${min}:${sec}`;
+}
+
+setInterval(() => {
+    seekBar.value = music.currentTime;
+    currentTime.innerHTML = formatTime(music.currentTime);
+    if(Math.floor(music.currentTime) === Math.floor(seekBar.max)){
+        forwardBtn.click();
+    }
+}, 500);
+
+// ---------Делаем перемещение времени музыки по бару---------
+
+seekBar.addEventListener('change', () => {
+    music.currentTime = seekBar.value;
+});
+
+// -------------Функция для удаления и добавления активных классов при переключении на другие песни------------
+
+const playMusic = () => {
+    music.play();
+    playBtn.classList.remove('pause');
+    disk.classList.add('play');
+};
