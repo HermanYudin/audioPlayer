8c6bd7a453d823f93296a7a52bf43c3b15c39d33
HermanYudin <hermanhockey1994@yandex.ru>
2023-09-20 09:29:31 +0300

    feat:duration edited

--- audioPlayer/index.js
+++ audioPlayer/index.js
@@ -110,6 +110,9 @@ const setMusic = function(i) {
     setTimeout(function(){//без этого считать продолжительность песни не будет
     seekBar.max = music.duration;
     musicDuration.innerHTML = formatTime(music.duration);
+    if(musicDuration.innerHTML === NaN){
+        musicDuration.innerHTML === formatTime(music.duration);
+    }
     },300);
 }
 
