5566d120c8ad8b2b2e027aba37413889cb9e34f0
HermanYudin <hermanhockey1994@yandex.ru>
2023-09-20 04:34:41 +0300

    feat: js was reworked in one file to easily download objects from array (Wed,Sep 20, 2023, 4:34 Am)

--- audioPlayer/index.js
+++ audioPlayer/index.js
@@ -11,6 +11,53 @@ const forwardBtn = document.querySelector('.forward-btn');
 const backwardBtn = document.querySelector('.backward-btn');
 
 
+let songs = [
+    {
+        name: 'Big Guns',
+        path: 'music/Skid Row - Big Guns.mp3',
+        artist: 'Skid Row',
+        cover: 'covers/skid.jpg'
+    },
+    {
+        name: 'The RocknRoll Qeen',
+        path: 'music/The Subways - The RocknRoll Qeen.mp3',
+        artist: 'The Subways',
+        cover: 'covers/sub.jpg'
+    },
+    {
+        name: 'I Got Mine',
+        path: 'music/The Black Keys - I Got Mine.mp3',
+        artist: 'The Black Keys',
+        cover: 'covers/black.jpg'
+    },
+    {
+        name: 'Time of Dying',
+        path: 'music/Three Days Grace - Time of Dying.mp3',
+        artist: 'Three Days Grace',
+        cover: 'covers/time.jpg'
+    },
+    {
+        name: 'Сентябрь Горит',
+        path: 'music/Stigmata - Сентябрь Горит.mp3',
+        artist: 'Stigmata',
+        cover: 'covers/sept.jpg'
+    },
+    {
+        name: 'Wings of a Butterfly',
+        path: 'music/HIM - Wings of a Butterfly.mp3',
+        artist: 'HIM',
+        cover: 'covers/him.jpg'
+    },
+    {
+        name: 'Psychosocial',
+        path: 'music/Slipknot - Psychosocial.mp3',
+        artist: 'Slipknot',
+        cover: 'covers/slip.jpg'
+    },
+    
+];
+
+
 // --------------Кликеры---------------
 
 playBtn.addEventListener('click', function(){
@@ -63,7 +110,7 @@ const setMusic = function(i) {
     setTimeout(function(){//без этого считать продолжительность песни не будет
     seekBar.max = music.duration;
     musicDuration.innerHTML = formatTime(music.duration);
-    },100);
+    },300);
 }
 
 setMusic(0);
